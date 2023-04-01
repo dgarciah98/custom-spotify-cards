37f47acbc7e498d632d31046ee3f51ff2a389b05
dgarciah98 <33066575+dgarciah98@users.noreply.github.com>
2023-04-01 11:32:10 +0000

    pages setup

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
-# export CLIENT_ID=YOUR_CLIENT_ID
-# export CLIENT_SECRET=YOUR_CLIENT_SECRET
-
-(trap 'kill 0' SIGINT; \
- bash -c 'cd frontend; trunk serve')
